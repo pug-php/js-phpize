31cea144ba8fa3aab550c12ff4c47850dd513d38
Kyle <kylekatarnls@gmail.com>
2016-08-15 09:57:46 +0200

    Allow to pass variables

--- src/JsPhpize/JsPhpize.php
+++ src/JsPhpize/JsPhpize.php
@@ -28,6 +28,11 @@ class JsPhpize
      */
     protected $dependencies = array();
 
+    /**
+     * @var array
+     */
+    protected $sharedVariables = array();
+
     public function __construct(array $options = array())
     {
         $this->options = $options;
@@ -92,8 +97,8 @@ class JsPhpize
     /**
      * Compile without the dependencies.
      *
-     * @param string $input             file or content
-     * @param string $filename          if specified, input is used as content and filename as its name
+     * @param string $input    file or content
+     * @param string $filename if specified, input is used as content and filename as its name
      *
      * @return string
      */
@@ -120,13 +125,18 @@ class JsPhpize
     /**
      * Compile and return the code execution result.
      *
-     * @param string $input             file or content
-     * @param string $filename          if specified, input is used as content and filename as its name
+     * @param string $input     file or content
+     * @param string $filename  if specified, input is used as content and filename as its name
+     * @param array  $variables variables to be used in rendered code
      *
      * @return mixed
      */
-    public function render($input, $filename = null)
+    public function render($input, $filename = null, array $variables = array())
     {
+        if (is_array($filename)) {
+            $variables = $filename;
+            $filename = null;
+        }
         if (!in_array($this->stream, $this->streamsRegistered)) {
             $this->streamsRegistered[] = $this->stream;
             if (in_array($this->stream, stream_get_wrappers())) {
@@ -136,6 +146,7 @@ class JsPhpize
             stream_wrapper_register($this->stream, $classParts[0] . '\Stream\ExpressionStream');
         }
 
+        extract(array_merge($this->sharedVariables, $variables));
         try {
             return include $this->stream . '://data;<?php ' . $this->compile($input, $filename);
         } catch (\JsPhpize\Compiler\Exception $e) {
@@ -156,24 +167,53 @@ class JsPhpize
     /**
      * Render a file.
      *
-     * @param string $file input file
+     * @param string $file      input file
+     * @param array  $variables variables to be used in rendered code
      *
      * @return string
      */
-    public function renderFile($file)
+    public function renderFile($file, array $variables = array())
     {
-        return $this->render(file_get_contents($file), $file);
+        return $this->render(file_get_contents($file), $file, $variables);
     }
 
     /**
      * Render raw code.
      *
-     * @param string $code input code
+     * @param string $code      input code
+     * @param array  $variables variables to be used in rendered code
      *
      * @return string
      */
-    public function renderCode($code)
+    public function renderCode($code, array $variables = array())
     {
-        return $this->compile($code, 'source.js');
+        return $this->compile($code, 'source.js', $variables);
+    }
+
+    /**
+     * Add a variable or an array of variables to be shared with all templates that will be rendered
+     * by the instance of Pug.
+     *
+     * @param array|string $variables|$key an associatives array of variable names and values, or a
+     *                                     variable name if you wish to sahre only one
+     * @param mixed        $value          if you pass an array as first argument, the second
+     *                                     argument will be ignored, else it will used as the
+     *                                     variable value for the variable name you passed as first
+     *                                     argument
+     */
+    public function share($variables, $value = null)
+    {
+        if (!is_array($variables)) {
+            $variables = array(strval($variables) => $value);
+        }
+        $this->sharedVariables = array_merge($this->sharedVariables, $variables);
+    }
+
+    /**
+     * Remove all previously set shared variables.
+     */
+    public function resetSharedVariables()
+    {
+        $this->sharedVariables = array();
     }
 }
