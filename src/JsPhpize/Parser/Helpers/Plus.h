05e254470d6e9e966a94907580b33ef5c736ea2f
Kyle <kylekatarnls@gmail.com>
2016-08-18 08:49:42 +0200

    Fix plus helper

--- src/JsPhpize/Parser/Helpers/Plus.h
+++ src/JsPhpize/Parser/Helpers/Plus.h
@@ -1,6 +1,6 @@
 function ($base) {
     foreach (array_slice(func_get_args(), 1) as $value) {
-        $base = is_string($value) || is_string($value) ? $base . $value : $base + $value;
+        $base = is_string($base) || is_string($value) ? $base . $value : $base + $value;
     }
 
     return $base;
